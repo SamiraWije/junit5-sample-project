4c247f39d6afeac7a81f2900e1ccb7596083133c
unknown <sameerawijesekara8@gmail.com>
2023-08-26 19:34:24 +0530

    null exceptions for last name and phone number

--- src/test/java/com/programming/techie/ContactManagerTest.java
+++ src/test/java/com/programming/techie/ContactManagerTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 class ContactManagerTest {
 
     @Test
+    @DisplayName("Contact Should Be Created")
     public void shouldCreateContact() {
         ContactManager contactManager = new ContactManager();
         contactManager.addContact("Aston", "Brown", "0123456789");
@@ -22,6 +23,24 @@ class ContactManagerTest {
         ContactManager contactManager = new ContactManager();
         Assertions.assertThrows(RuntimeException.class, () -> {
             contactManager.addContact(null, "Brown", "0123456789");
+        });
+    }
+
+    @Test
+    @DisplayName("Should Not Create Contact When Last Name is Null")
+    public void shouldThrowRuntimeExceptionWhenLastNameIsNull() {
+        ContactManager contactManager = new ContactManager();
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            contactManager.addContact("Aston", null, "0123456789");
+        });
+    }
+
+    @Test
+    @DisplayName("Should Not Create Contact When Phone Number is Null")
+    public void shouldThrowRuntimeExceptionWhenPhoneNumberIsNull() {
+        ContactManager contactManager = new ContactManager();
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            contactManager.addContact("Aston", "Brown", null);
         });
     }
 }
