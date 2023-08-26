7a6d65a6ad9d1c6b641309893a7ff2c2e956a0eb
unknown <sameerawijesekara8@gmail.com>
2023-08-26 17:51:07 +0530

    first name null exception

--- README.md
+++
@@ -1,7 +0,0 @@
-<<<<<<< HEAD
-# contact-manager-starter
-Starter Project for the Junit 5 Tutorial
-=======
-# junit5-sample-project
-JUnit 5 sample project
->>>>>>> 9edaff3569d7f9a000c6827cc00e45edaa421b35
--- src/test/java/com/programming/techie/ContactManagerTest.java
+++ src/test/java/com/programming/techie/ContactManagerTest.java
@@ -1,9 +1,10 @@
 package com.programming.techie;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+//import static org.junit.jupiter.api.Assertions.*;
 
 class ContactManagerTest {
 
@@ -13,5 +14,14 @@ class ContactManagerTest {
         contactManager.addContact("Aston", "Brown", "0123456789");
         Assertions.assertFalse(contactManager.getAllContacts().isEmpty());
         Assertions.assertEquals(1,contactManager.getAllContacts().size());
+    }
+
+    @Test
+    @DisplayName("Should Not Create Contact When First Name is Null")
+    public void shouldThrowRuntimeExceptionWhenFirstNameIsNull() {
+        ContactManager contactManager = new ContactManager();
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            contactManager.addContact(null, "Brown", "0123456789");
+        });
     }
 }
